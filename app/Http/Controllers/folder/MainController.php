eb39db532c5fe689730655f5c52a5ddc6319ba08
gabrielemafrica <gabriele.ga@live.it>
2023-09-04 17:23:36 +0300

    show whit friends

--- app/Http/Controllers/folder/MainController.php
+++ app/Http/Controllers/folder/MainController.php
@@ -18,7 +18,102 @@ class MainController extends Controller
     }
     public function show($id) {
         $person = Person :: find($id);
-        // dd($person);
-        return view("people.show", compact('person'));
+
+        //friends
+        $friends = $person->friends;
+
+        //friendships
+        $friendship = $person->friendships;
+
+        // Trova gli amici degli amici
+        $friendOfFriend =  Person::select('people.id', 'people.firstName', 'people.surname', 'people.gender')
+                                    ->join('connections AS C1', 'people.id', '=', 'C1.person_id')
+                                    ->join('connections AS C2', 'C1.friend_id', '=', 'C2.person_id')
+                                    ->where('C2.friend_id', $id)
+                                    ->where('people.id', '<>', $id)
+                                    ->whereNotIn('people.id', function ($query) use ($id) {
+                                        $query->select('friend_id')
+                                            ->from('connections')
+                                            ->where('person_id', $id);
+                                        })
+                                    ->distinct()
+                                    ->get();
+
+            //query corretta SQL
+        // SELECT DISTINCT
+        // P.id AS fof_id,
+        // P.firstName AS fof_firstName,
+        // P.surname AS fof_surname
+        // FROM people AS P
+        // JOIN connections AS C1 ON P.id = C1.person_id
+        // JOIN connections AS C2 ON C1.friend_id = C2.person_id
+        // WHERE C2.friend_id = 1
+        // AND P.id <> 1
+        // AND P.id NOT IN (
+        //     SELECT friend_id FROM connections WHERE person_id = 1
+        // );
+
+        //CONSIGLIATI
+        $friendsSuggested = Person::select('people.id', 'people.firstName', 'people.surname', 'people.gender')
+                                    ->join('connections AS C1', 'people.id', '=', 'C1.person_id')
+                                    ->join('connections AS C2', 'C1.friend_id', '=', 'C2.person_id')
+                                    ->where('C2.friend_id', $id)
+                                    ->where('people.id', '<>', $id)
+                                    ->whereNotIn('people.id', function ($query) use ($id) {
+                                        $query->select('friend_id')
+                                            ->from('connections')
+                                            ->where('person_id', $id);
+                                    })
+                                    ->whereNotIn('people.id', function ($query) use ($id) {
+                                        $query->select('friend_id')
+                                            ->from('connections')
+                                            ->where('person_id', $id);
+                                    })
+                                    ->whereIn('people.id', function ($subquery) use ($id) {
+                                        $subquery->select('C3.person_id')
+                                            ->from('connections AS C3')
+                                            ->whereIn('C3.friend_id', function ($innerSubquery) use ($id) {
+                                                $innerSubquery->select('C4.friend_id')
+                                                    ->from('connections AS C4')
+                                                    ->where('C4.person_id', $id);
+                                            })
+                                            ->groupBy('C3.person_id')
+                                            ->havingRaw('COUNT(C3.friend_id) >= 2');
+                                    })
+                                    ->distinct()
+                                    ->get();
+
+
+
+        // SQL
+        // SELECT DISTINCT
+        // P.id,
+        // P.firstName,
+        // P.surname
+        // FROM people AS P
+        // JOIN connections AS C1 ON P.id = C1.person_id
+        // JOIN connections AS C2 ON C1.friend_id = C2.person_id
+        // WHERE C2.friend_id = 20
+        // AND P.id <> 20
+        // AND P.id NOT IN (
+        // SELECT friend_id FROM connections WHERE person_id = 20
+        // )
+        // AND P.id NOT IN (
+        // SELECT friend_id FROM connections WHERE person_id = 20
+        // )
+        // AND P.id IN (
+        // SELECT C3.person_id
+        // FROM connections AS C3
+        // WHERE C3.friend_id IN (
+        //     SELECT C4.friend_id
+        //     FROM connections AS C4
+        //     WHERE C4.person_id = 20
+        // )
+        // GROUP BY C3.person_id
+        // HAVING COUNT(C3.friend_id) >= 2
+        // );
+
+
+        return view("people.show", compact('person', 'friends', 'friendOfFriend', 'friendsSuggested'));
     }
 }
--- resources/views/people/show.blade.php
+++ resources/views/people/show.blade.php
@@ -9,20 +9,84 @@
         <h1>USER ID: {{ $person->id }}</h1>
         <div class="container d-flex justify-content-center my-4">
 
-
             <div class="card {{ $person->gender == 'male' ? 'text-bg-primary' : 'text-bg-danger' }}" style="width: 18rem;">
                 <div class="card-header">
                     {{ $person->firstName }} {{ $person->surname }}
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">age: {{ $person->age }}</li>
-                    <li class="list-group-item">gender: {{ $person->gender }}</li>
+                    <li class="list-group-item">age: {{ $person->age ? $person->age : 'not set' }}</li>
+                    <li class="list-group-item">gender: {{ $person->gender ? $person->gender : 'not set' }}</li>
+
                 </ul>
             </div>
 
+        </div>
 
+        <div class="conttainer">
+            <a href="{{ route('homepage') }}" class="btn btn-warning">
+                HOME
+            </a>
+        </div>
 
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="container mt-4">
+                        <h2>Friends</h2>
+                        <ul class="my-4">
+                            @foreach ($friends as $friend)
+                                <li class="list-group-item my-2">
+                                    <a href="{{ route('person.show', $friend->id) }}"
+                                        class="btn {{ $friend->gender == 'male' ? 'btn-primary' : 'btn-danger' }}">
+                                        ID: {{ $friend->id }}
+                                        <br>
+                                        {{ $friend->firstName }} {{ $friend->surname }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
 
+                <div class="col-4">
+                    <div class="container mt-4">
+                        <h2>Friends of Friend</h2>
+                        <ul class="my-4">
+                            @foreach ($friendOfFriend as $fof)
+                                <li class="list-group-item my-2">
+                                    <a href="{{ route('person.show', $fof->id) }}"
+                                        class="btn {{ $fof->gender == 'male' ? 'btn-primary' : 'btn-danger' }}">
+                                        ID: {{ $fof->id }}
+                                        <br>
+                                        {{ $fof->firstName }} {{ $fof->surname }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
 
+                <div class="col-4">
+                    <div class="container mt-4">
+                        <h2>Friends Suggesteds</h2>
+                        <ul class="my-4">
+                            @if ($friendsSuggested->isEmpty())
+                                <h5>No suggestions</h5>
+                            @else
+                                @foreach ($friendsSuggested as $fs)
+                                    <li class="list-group-item my-2">
+                                        <a href="{{ route('person.show', $fs->id) }}"
+                                            class="btn {{ $fs->gender == 'male' ? 'btn-primary' : 'btn-danger' }}">
+                                            ID: {{ $fs->id }}
+                                            <br>
+                                            {{ $fs->firstName }} {{ $fs->surname }}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     @endsection
